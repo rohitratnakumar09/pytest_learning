6dcd3954cf50743091d7505ffd143048af5c6f61
rohitratnakumar <rohitratnakumar>
2025-07-12 11:33:50 +0530

    chnages for running multiple sites with different conftests

--- selenuim_python_framework/tests/conftest.py
+++ selenuim_python_framework/tests/conftest.py
@@ -9,24 +9,24 @@ from utils.helper import ConfigUtility
 from utils.driver_utility import WebDriver
 
 
-@pytest.fixture(scope="session")
-def configs():
-    """
-    Load the configuration file once per test session.
-    """
-    config = ConfigUtility('goibibo')
-    return config.load_config_file()
-
-
-@pytest.fixture(scope="session", autouse=True)
-def set_env_variables(configs):
-    """
-    Sets environment variables for the test session.
-    Cleans up after the session ends.
-    """
-    os.environ["TEST_FOLDER"] = configs.get('PROD', 'folder')
-    yield
-    os.environ.pop("TEST_FOLDER", None)
+# @pytest.fixture(scope="session")
+# def configs():
+#     """
+#     Load the configuration file once per test session.
+#     """
+#     config = ConfigUtility('goibibo')
+#     return config.load_config_file()
+#
+#
+# @pytest.fixture(scope="session", autouse=True)
+# def set_env_variables(configs):
+#     """
+#     Sets environment variables for the test session.
+#     Cleans up after the session ends.
+#     """
+#     os.environ["TEST_FOLDER"] = configs.get('PROD', 'folder')
+#     yield
+#     os.environ.pop("TEST_FOLDER", None)
 
 
 def pytest_addoption(parser):
---
+++ selenuim_python_framework/tests/goibibo_test/conftest.py
@@ -0,0 +1,25 @@
+import os
+import pytest
+from common.constants import *
+from utils.helper import ConfigUtility
+from utils.driver_utility import WebDriver
+
+
+@pytest.fixture(scope="session")
+def configs():
+    """
+    Load the configuration file once per test session.
+    """
+    config = ConfigUtility('goibibo')
+    return config.load_config_file()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def set_env_variables(configs):
+    """
+    Sets environment variables for the test session.
+    Cleans up after the session ends.
+    """
+    os.environ["TEST_FOLDER"] = configs.get('PROD', 'folder')
+    yield
+    os.environ.pop("TEST_FOLDER", None)
